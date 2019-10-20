d93bc08009b0423c5341eb95944256b8c89186e4
amour-youcef <amour-youcef@hotmail.com>
2019-10-20 19:38:26 +0200

    maj final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPreCompComponent } from './mon-pre-comp/mon-pre-comp.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { AppareilComponent } from './openclassroom/appareil/appareil.component';
+import {AppareilService} from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component'
+import { Routes, RouterModule } from '@angular/router';
+import {AuthService} from './services/auth.service';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
 
+const appRoutes: Routes = [
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
-  declarations: [AppComponent, MonPreCompComponent, PostCreateComponent, AppareilComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  declarations: [AppComponent, MonPreCompComponent, PostCreateComponent, AppareilComponent, AuthComponent, AppareilViewComponent, SingleAppareilComponent, FourOhFourComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [AppareilService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/openclassroom/appareil/appareil.component.ts
+++ src/app/openclassroom/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit ,Input} from '@angular/core';
+import { AppareilService } from 'src/app/services/appareil.service';
 
 @Component({
   selector: 'app-appareil',
@@ -8,10 +9,12 @@ import { Component, OnInit ,Input} from '@angular/core';
 export class AppareilComponent implements OnInit {
   @Input() appareilName: string;
   @Input() appareilStatus: string;
+  @Input() index: number;
+  @Input() id: number;
 
 
 
-  constructor() { }
+constructor(private appareilService: AppareilService) { }
 
   ngOnInit() {
   }
@@ -26,4 +29,14 @@ export class AppareilComponent implements OnInit {
       return 'red';
     }
 }
+
+
+onSwitch() {
+  if(this.appareilStatus === 'allumé') {
+    this.appareilService.switchOffOne(this.index);
+  } else if(this.appareilStatus === 'éteint') {
+    this.appareilService.switchOnOne(this.index);
+  }
+}
+
 }
