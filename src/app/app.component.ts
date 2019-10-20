eb5326ac0d7dff13a6c7f2c147497e84f792f3cf
amour-youcef <amour-youcef@hotmail.com>
2019-10-20 10:59:53 +0200

    maj-angular-dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app-tuto';
+  isAuth = false;
+
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer() {
+    console.log('On allume tout !');
 }
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPreCompComponent } from './mon-pre-comp/mon-pre-comp.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { AppareilComponent } from './openclassroom/appareil/appareil.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MonPreCompComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MonPreCompComponent, PostCreateComponent, AppareilComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
